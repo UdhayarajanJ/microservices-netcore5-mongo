9b9407444b19d694506b1f4cf0377ccaf4b6919d
Udhayarajan J <udhayarajan.j@shauryatechnosoft.com>
2022-11-03 10:05:43 +0530

    Candidate Job Apply Test Case Completed

--- api.test.candidate/Mocks/MockCanidateJobController.cs
+++ api.test.candidate/Mocks/MockCanidateJobController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using app.canditates.api.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,21 @@ using System.Threading.Tasks;
 
 namespace api.test.candidate.Mocks
 {
-    class MockCanidateJobController
+    public class MockCanidateJobController
     {
+        public CandidateApplyJobs GetMockDataCandidateApplyJobs()
+        {
+            return new CandidateApplyJobs()
+            {
+                documentObjectId = string.Empty,
+                candidateId = Guid.NewGuid().ToString(),
+                isDeleted = false,
+                createdDate = DateTime.Now,
+                id = Guid.NewGuid().ToString(),
+                jobApplyDate = DateTime.Now,
+                jobPostId = Guid.NewGuid().ToString(),
+                lastUpdate=DateTime.Now
+            };
+        }
     }
 }
--- api.test.candidate/TestControllers/TestCandidateJobController.cs
+++ api.test.candidate/TestControllers/TestCandidateJobController.cs
@@ -1,12 +1,80 @@
-﻿using System;
+﻿using api.test.candidate.Mocks;
+using app.canditates.api.Controllers;
+using app.canditates.api.IRepository;
+using app.canditates.api.Models;
+using app.utility.microservice.Models;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace api.test.candidate.TestControllers
 {
-    class TestCandidateJobController
+    public class TestCandidateJobController
     {
+        private Mock<ICandidateApplyJobRepository> _mockCandidateApplyJobRepository;
+        private MockCanidateJobController _mockCanidateJobController;
+        public TestCandidateJobController()
+        {
+            _mockCandidateApplyJobRepository = new Mock<ICandidateApplyJobRepository>();
+            _mockCanidateJobController = new MockCanidateJobController();
+        }
+        /// <summary>
+        /// To Test The Apply For Job Candidates Should Case Passed And Apply Jobs
+        /// </summary>
+        /// <returns></returns>
+        [Fact(DisplayName = "To_Test_ApplyCandidateJobs_ShouldBe_Applied")]
+        [Trait("CandidateJobs", "To_Test_ApplyCandidateJobs_ShouldBe_Applied")]
+        private async Task To_Test_ApplyCandidateJobs_ShouldBe_Applied()
+        {
+            //Arrange
+            long result = 1;
+            int expectedStatusCode = 200;
+            CandidateApplyJobs candidateApplyJobs = _mockCanidateJobController.GetMockDataCandidateApplyJobs();
+            _mockCandidateApplyJobRepository.Setup(x => x.CandidateApplyJobs(candidateApplyJobs)).Returns(Task.FromResult(result));
+
+            //Act
+            CanditatesJobController canditatesJobController = new CanditatesJobController(_mockCandidateApplyJobRepository.Object);
+            var callAPI = canditatesJobController.CandidateApplyJobs(candidateApplyJobs);
+            var actionResult = await callAPI as OkObjectResult;
+            CommonApiReponse apiReponse = (CommonApiReponse)actionResult.Value;
+
+            // Assert
+            apiReponse.Should().NotBeNull();
+            apiReponse.statusCode.Should().Be(expectedStatusCode);
+            apiReponse.statusMessage.Should().BeEquivalentTo("Job Applied Successfully...");
+            apiReponse.responseData.Should().Be(result);
+        }
+        /// <summary>
+        /// To Test The Apply For Job Candidates Should Not Case Passed And Apply Jobs
+        /// </summary>
+        /// <returns></returns>
+        [Fact(DisplayName = "To_Test_ApplyCandidateJobs_ShouldNotBe_Applied")]
+        [Trait("CandidateJobs", "To_Test_ApplyCandidateJobs_ShouldNotBe_Applied")]
+        private async Task To_Test_ApplyCandidateJobs_ShouldNotBe_Applied()
+        {
+            //Arrange
+            long result = 0;
+            int expectedStatusCode = 404;
+            CandidateApplyJobs candidateApplyJobs = _mockCanidateJobController.GetMockDataCandidateApplyJobs();
+            _mockCandidateApplyJobRepository.Setup(x => x.CandidateApplyJobs(candidateApplyJobs)).Returns(Task.FromResult(result));
+
+            //Act
+            CanditatesJobController canditatesJobController = new CanditatesJobController(_mockCandidateApplyJobRepository.Object);
+            var callAPI = canditatesJobController.CandidateApplyJobs(candidateApplyJobs);
+            var actionResult = await callAPI as OkObjectResult;
+            CommonApiReponse apiReponse = (CommonApiReponse)actionResult.Value;
+
+            // Assert
+            apiReponse.Should().NotBeNull();
+            apiReponse.statusCode.Should().Be(expectedStatusCode);
+            apiReponse.statusMessage.Should().BeEquivalentTo("Job Applied Failed...");
+            apiReponse.responseData.Should().BeNull();
+        }
     }
 }
